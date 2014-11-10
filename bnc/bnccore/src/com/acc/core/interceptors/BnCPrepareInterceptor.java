6e40532f52f2779783038e6b96efb5d3354a8a3c
Prasun07 <prasun.a.kumar@accenture.com>
2014-11-10 11:15:07 +0530

    Code committing to save customer collect order.
    Code committing to save customer collect order.
    creating service and integrate it with Interceptor, also add spring.xml
    file of bnccore extension.

--- bnc/bnccore/src/com/acc/core/interceptors/BnCPrepareInterceptor.java
+++ bnc/bnccore/src/com/acc/core/interceptors/BnCPrepareInterceptor.java
@@ -4,6 +4,7 @@
 package com.acc.core.interceptors;
 
 import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.servicelayer.config.ConfigurationService;
 import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
 import de.hybris.platform.servicelayer.interceptor.InterceptorException;
 import de.hybris.platform.servicelayer.interceptor.PrepareInterceptor;
@@ -13,7 +14,10 @@ import java.security.NoSuchAlgorithmException;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.acc.core.model.CollectOrderModel;
+import com.acc.core.services.collectorder.CustomerCollectOrderService;
 import com.acc.core.util.UCOIDUtility;
 
 
@@ -25,6 +29,10 @@ public class BnCPrepareInterceptor implements PrepareInterceptor
 {
 	private static final String PICKUP_GROSS = "pickup-gross";
 	private static final Logger LOG = Logger.getLogger(BnCPrepareInterceptor.class);
+	@Autowired
+	private CustomerCollectOrderService customerCollectOrderService;
+	@Autowired
+	private ConfigurationService configurationService;
 
 	@Override
 	public void onPrepare(final Object model, final InterceptorContext ctx) throws InterceptorException
@@ -37,11 +45,35 @@ public class BnCPrepareInterceptor implements PrepareInterceptor
 			{
 				order.setUCOID(new UCOIDUtility().getUCOID(order.getUser().getUid(), order.getCode()));
 				LOG.info("## Modified Order Number " + order.getUCOID() + " added UCOID ##");
+
+				saveColelctOrder(order, ctx);
 			}
 			catch (NoSuchAlgorithmException | InvalidKeyException e)
 			{
 				LOG.error(e.getMessage(), e);
 			}
 		}
+	
+	}
+	
+	/**
+	 * This helper method is used to save the Customer collect order if he/she choose delivery mode as
+	 * "Pickup from next counter".
+	 * 
+	 * @param order
+	 *           Indicates Order Model.
+	 * @param ctx
+	 *           Indicates Interceptor context.
+	 */
+	private void saveColelctOrder(final OrderModel order, final InterceptorContext ctx)
+	{
+		final CollectOrderModel collectOrderModel = ctx.getModelService().create(CollectOrderModel.class);
+		collectOrderModel.setCID(order.getUser().getUid());
+		collectOrderModel.setOID(order.getCode());
+		collectOrderModel.setUCOID(order.getUCOID());
+		collectOrderModel.setStatus(configurationService.getConfiguration().getString("order.status.pending", ""));
+		customerCollectOrderService.saveCustomerColectOrder(collectOrderModel);
+		LOG.info("Customer Collect Order data saved successfully from.");
+
 	}
 }
